73795ea4c15e99acc56af135347694d7e8047723
lohargaurav00 <lohargaurav00@gmail.com>
2023-12-16 14:41:26 +0530

    + delete_quote

--- src/handlers.rs
+++ src/handlers.rs
@@ -102,3 +102,22 @@ pub async fn update_quote(
         Err(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
     }
 }
+
+pub async fn delete_quote(
+    extract::State(pool): extract::State<PgPool>,
+    extract::Path(id): extract::Path<Uuid>,
+) -> http::StatusCode {
+    let res = sqlx::query("DELETE FROM quotes WHERE id = $1")
+        .bind(&id)
+        .execute(&pool)
+        .await
+        .map(|res| match res.rows_affected() {
+            0 => http::StatusCode::NOT_FOUND,
+            _ => http::StatusCode::OK,
+        });
+
+    match res {
+        Ok(status) => status,
+        Err(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
